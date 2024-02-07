543a2b4e0216a8e9193b2648fb6151c4214ec097
Enzo Bozzani <bozzanienzo@gmail.com>
2024-02-07 01:40:44 -0300

    feat: add OAuth (google & github)

--- auth.config.ts
+++ auth.config.ts
@@ -4,9 +4,19 @@ import bcrypt from 'bcryptjs';
 
 import { LoginSchema } from '@/schemas';
 import { getUserByEmail } from './data/user';
+import GitHub from 'next-auth/providers/github';
+import Google from 'next-auth/providers/google';
 
 export default {
 	providers: [
+		Google({
+			clientId: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+		}),
+		GitHub({
+			clientId: process.env.GITHUB_CLIENT_ID,
+			clientSecret: process.env.GITHUB_CLIENT_SECRET,
+		}),
 		Credentials({
 			async authorize(credentials, request) {
 				const validatedFields = LoginSchema.safeParse(credentials);
