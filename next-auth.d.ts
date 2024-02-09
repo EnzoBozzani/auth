5e33e12c535c79de8bece95b4c2473f7acea065f
Enzo Bozzani <bozzanienzo@gmail.com>
2024-02-09 16:56:00 -0300

    feat: develop server and client example for using current user data

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -2,7 +2,7 @@ import NextAuth, { DefaultSession } from 'next-auth';
 import { JWT } from 'next-auth/jwt';
 import { UserRole } from '@prisma/client';
 
-export type ExtendedUser = DefaultSession['user'] & { role: UserRole };
+export type ExtendedUser = DefaultSession['user'] & { role: UserRole; isTwoFactorEnabled: boolean };
 
 declare module 'next-auth' {
 	interface Session {
