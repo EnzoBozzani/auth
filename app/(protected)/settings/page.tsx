2d8e86bb2adecadadcc6e44914ca981bf2588310
Enzo Bozzani <bozzanienzo@gmail.com>
2024-02-11 04:54:43 -0300

    feat: develop settings page

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -6,15 +6,18 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useTransition, useState } from 'react';
 import { useSession } from 'next-auth/react';
 
+import { Switch } from '@/components/ui/switch';
 import { settings } from '@/actions/settings';
 import { Button } from '@/components/ui/button';
 import { Card, CardHeader, CardContent } from '@/components/ui/card';
-import { Form, FormField, FormControl, FormItem, FormLabel, FormDescription, FormMessage } from '@/components/ui/form';
+import { Form, FormField, FormControl, FormItem, FormLabel, FormMessage, FormDescription } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { SettingsSchema } from '@/schemas';
 import { useCurrentUser } from '@/hooks/useCurrentUser';
 import { FormSuccess } from '@/components/FormSuccess';
 import { FormError } from '@/components/FormError';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { UserRole } from '@prisma/client';
 
 const SettingsPage = () => {
 	const user = useCurrentUser();
@@ -29,10 +32,16 @@ const SettingsPage = () => {
 		defaultValues: {
 			name: user?.name || undefined,
 			email: user?.email || undefined,
+			password: undefined,
+			newPassword: undefined,
+			role: user?.role || undefined,
+			isTwoFactorEnabled: user?.isTwoFactorEnabled || undefined,
 		},
 	});
 
 	const onSubmit = (values: z.infer<typeof SettingsSchema>) => {
+		setError('');
+		setSuccess('');
 		startTransition(() => {
 			settings(values)
 				.then((data) => {
@@ -74,26 +83,116 @@ const SettingsPage = () => {
 												disabled={isPending}
 											/>
 										</FormControl>
+										<FormMessage />
 									</FormItem>
 								)}
 							/>
+							{user?.isOAuth === false && (
+								<>
+									<FormField
+										control={form.control}
+										name='email'
+										render={({ field }) => (
+											<FormItem>
+												<FormLabel>Email</FormLabel>
+												<FormControl>
+													<Input
+														{...field}
+														placeholder='johndoe@example.com'
+														disabled={isPending}
+														type='email'
+													/>
+												</FormControl>
+												<FormMessage />
+											</FormItem>
+										)}
+									/>
+									<FormField
+										control={form.control}
+										name='password'
+										render={({ field }) => (
+											<FormItem>
+												<FormLabel>Password</FormLabel>
+												<FormControl>
+													<Input
+														{...field}
+														placeholder='******'
+														disabled={isPending}
+														type='password'
+													/>
+												</FormControl>
+												<FormMessage />
+											</FormItem>
+										)}
+									/>
+									<FormField
+										control={form.control}
+										name='newPassword'
+										render={({ field }) => (
+											<FormItem>
+												<FormLabel>New Password</FormLabel>
+												<FormControl>
+													<Input
+														{...field}
+														placeholder='******'
+														disabled={isPending}
+														type='password'
+													/>
+												</FormControl>
+												<FormMessage />
+											</FormItem>
+										)}
+									/>
+								</>
+							)}
 							<FormField
 								control={form.control}
-								name='email'
+								name='role'
 								render={({ field }) => (
 									<FormItem>
-										<FormLabel>Email</FormLabel>
-										<FormControl>
-											<Input
-												{...field}
-												placeholder='johndoe@example.com'
-												disabled={isPending}
-												type='email'
-											/>
-										</FormControl>
+										<FormLabel>Role</FormLabel>
+										<Select
+											disabled={isPending}
+											onValueChange={field.onChange}
+											defaultValue={field.value}
+										>
+											<FormControl>
+												<SelectTrigger>
+													<SelectValue placeholder='Select a role' />
+												</SelectTrigger>
+											</FormControl>
+											<SelectContent>
+												<SelectItem value={UserRole.ADMIN}>Admin</SelectItem>
+												<SelectItem value={UserRole.USER}>User</SelectItem>
+											</SelectContent>
+										</Select>
+										<FormMessage />
 									</FormItem>
 								)}
 							/>
+							{user?.isOAuth === false && (
+								<>
+									<FormField
+										control={form.control}
+										name='isTwoFactorEnabled'
+										render={({ field }) => (
+											<FormItem className='flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm'>
+												<div className='space-y-0.5'>
+													<FormLabel>Two Factor Authentication</FormLabel>
+													<FormDescription>Enable Two Factor Authentication</FormDescription>
+												</div>
+												<FormControl>
+													<Switch
+														disabled={isPending}
+														checked={field.value}
+														onCheckedChange={field.onChange}
+													/>
+												</FormControl>
+											</FormItem>
+										)}
+									/>
+								</>
+							)}
 						</div>
 						<FormSuccess message={success} />
 						<FormError message={error} />
